2acd78948183db5e4586fbe406ede7b35ff03193
David <135177739+6dam@users.noreply.github.com>
2026-01-30 21:24:29 -0800

    worm

---
+++ ggj-2026/Scenes/worm.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+var left = true
+var SPEED = 200
+
+func _ready():
+	pass
+	
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.play()
+	
+	velocity.x = -SPEED if left else SPEED
+	
+	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	left = !left
+	if left: $AnimatedSprite2D.flip_h = false
+	else: $AnimatedSprite2D.flip_h = true
