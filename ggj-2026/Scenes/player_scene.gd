94addc98d50c09e1c495bfaea71103e257189556
LoganMoleman <logandhubert@gmail.com>
2026-01-30 21:44:06 -0800

    character controller and masks

--- ggj-2026/Scenes/player_scene.gd
+++ ggj-2026/Scenes/player_scene.gd
@@ -1,11 +1,36 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var leftMask
+var rightMask
+var leftMaskButtonTime = 0.0
+var rightMaskButtonTime = 0.0
+var buttonPressTime = 0.8
+@onready var leftMarker = $leftMarker2d
+@onready var rightMarker = $rightMarker2d
+
+func _ready() -> void:
+	main.player = self
 
 func _physics_process(delta: float) -> void:
+	#mask swapping logic - determines if the button has been held long enough and calls the corresponding func
+	if Input.is_action_pressed("mask_left"):
+		leftMaskButtonTime += delta
+	else:
+		leftMaskButtonTime = 0
+	if leftMaskButtonTime >= buttonPressTime:
+		_mask_swap(false)#false for left hand
+		leftMaskButtonTime = -buttonPressTime#additional button wait time after swapping
+	if Input.is_action_pressed("mask_right"):
+		rightMaskButtonTime += delta
+	else:
+		rightMaskButtonTime = 0
+	if rightMaskButtonTime >= buttonPressTime:
+		_mask_swap(true)#true for right hand
+		rightMaskButtonTime = -buttonPressTime#additional button wait time after swapping
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -23,3 +48,30 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+func _mask_use(mask):#func is given the mask name, and does the corresponding action
+	match mask.ability:
+		"none":
+			pass
+		"hover":
+			pass
+
+func _mask_swap(rightHanded):
+	var closestMask = get_closest_mask()
+	if rightHanded:
+		rightMask = closestMask
+		print("Right Swap")
+	if !rightHanded:
+		leftMask = closestMask
+		print("Left Swap")
+
+func get_closest_mask():
+	var overlapArray = $maskDetectorArea2D.get_overlapping_bodies()
+	var closestMask
+	var smallestDistance = 999999
+	for i in overlapArray.size():
+		if overlapArray[i].is_in_group("mask"):
+			if abs(global_position.length() - overlapArray[i].global_position.length()) < smallestDistance:
+				smallestDistance = abs(global_position.length() - overlapArray[i].global_position.length())
+				closestMask = overlapArray[i]
+	return closestMask
