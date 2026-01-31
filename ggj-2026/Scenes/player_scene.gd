795f21b1b20baf84996f3738e08055ca85323f0b
LoganMoleman <logandhubert@gmail.com>
2026-01-30 22:25:57 -0800

    mask buttons

--- ggj-2026/Scenes/player_scene.gd
+++ ggj-2026/Scenes/player_scene.gd
@@ -5,9 +5,6 @@ const JUMP_VELOCITY = -400.0
 
 var leftMask
 var rightMask
-var leftMaskButtonTime = 0.0
-var rightMaskButtonTime = 0.0
-var buttonPressTime = 0.8
 @onready var leftMarker = $leftMarker2d
 @onready var rightMarker = $rightMarker2d
 @onready var animatedSprite = $AnimatedSprite2D
@@ -16,23 +13,18 @@ func _ready() -> void:
 	main.player = self
 
 func _physics_process(delta: float) -> void:
-	#mask swapping logic - determines if the button has been held long enough and calls the corresponding func
-	if Input.is_action_pressed("mask_left"):
-		leftMaskButtonTime += delta
-	else:
-		leftMaskButtonTime = 0
-	if leftMaskButtonTime >= buttonPressTime:
+	#mask swapping inputs
+	if Input.is_action_just_pressed("swap_left"):
 		_mask_swap(false)#false for left hand
-		leftMaskButtonTime = -buttonPressTime#additional button wait time after swapping
-	if Input.is_action_pressed("mask_right"):
-		rightMaskButtonTime += delta
-	else:
-		rightMaskButtonTime = 0
-	if rightMaskButtonTime >= buttonPressTime:
+	if Input.is_action_just_pressed("swap_right"):
 		_mask_swap(true)#true for right hand
-		rightMaskButtonTime = -buttonPressTime#additional button wait time after swapping
+		
+	#Mask action inputs
+	if Input.is_action_just_released("mask_left"):
+		_mask_use(leftMask)
+	if Input.is_action_just_released("mask_right"):
+		_mask_use(rightMask)
 	
-	mask_updates()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -41,6 +33,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -62,13 +55,17 @@ func _physics_process(delta: float) -> void:
 		animatedSprite.flip_h = false
 
 	move_and_slide()
+	mask_updates()
 
 func _mask_use(mask):#func is given the mask name, and does the corresponding action
-	match mask.ability:
-		"none":
-			pass
-		"hover":
-			pass
+	if mask:
+		match mask.maskName:
+			"none":
+				pass
+			"hover":
+				pass
+			"Template":
+				velocity.y = JUMP_VELOCITY
 
 func _mask_swap(rightHanded):
 	var closestMask = get_closest_mask()
@@ -80,16 +77,19 @@ func _mask_swap(rightHanded):
 		print("Left Swap")
 
 func get_closest_mask():
-	var overlapArray = $maskDetectorArea2D.get_overlapping_bodies()
+	var overlapArray = $maskDetectorArea2D.get_overlapping_areas()
 	var closestMask
 	var smallestDistance = 999999
 	for i in overlapArray.size():
-		if overlapArray[i].is_in_group("mask"):
-			if abs(global_position.length() - overlapArray[i].global_position.length()) < smallestDistance:
-				smallestDistance = abs(global_position.length() - overlapArray[i].global_position.length())
-				closestMask = overlapArray[i]
+		if overlapArray[i] != leftMask && overlapArray[i] != rightMask:
+			if overlapArray[i].is_in_group("mask"):
+				if abs(global_position.length() - overlapArray[i].global_position.length()) < smallestDistance:
+					smallestDistance = abs(global_position.length() - overlapArray[i].global_position.length())
+					closestMask = overlapArray[i]
 	return closestMask
 
 func mask_updates():
 	if leftMask:
 		leftMask.global_position = leftMarker.global_position
+	if rightMask:
+		rightMask.global_position = rightMarker.global_position
